cc8cdc92557813c2acdf7536519c3e1085abf6f4
Conor6 <conorflood08@hotmail.com>
2022-01-26 11:34:14 +0000

    Addded server  config

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post('/addcourt', async(req, res) => {
+  try {
+
+    const { crt_name } = req.body;
+
+    const insert = await pool.query(
+      "INSERT INTO court (crt_name) VALUES($1)", 
+      [crt_name]
+    );
+
+    res.json(insert);
+
+    console.log(req.body);
+    
+  } 
+  catch (error) {
+    console.log(error.message);
+  }
+
+})
+
+
+
+
+
+app.listen(5001, function() {
+
+  console.log("Server running on port 5001");
+
+});
