b8b9a35e48a3133e280e6dbbafdfd8c9d6538003
Conor6 <conorflood08@hotmail.com>
2022-03-17 22:11:17 +0000

    Add Events to calendar

--- Front-End/client/src/Pages/Schedule.js
+++ Front-End/client/src/Pages/Schedule.js
@@ -3,34 +3,80 @@ import {useNavigate, useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
 import Paper from '@mui/material/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import { Scheduler, DayView,Appointments, } from '@devexpress/dx-react-scheduler-material-ui';
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
+import { Scheduler, DayView, WeekView, Appointments, AppointmentTooltip, ConfirmationDialog, } from '@devexpress/dx-react-scheduler-material-ui';
+import { AppointmentForm } from '@devexpress/dx-react-scheduler-material-ui';
 
 
-function Schedule({setAuth}) {
+function Schedule() {
 
-  const currentDate = '2018-11-01';
-  const schedulerData = [
-    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+  const appointments = [
+    { title: 'Mail New Leads for Follow Up', startDate: '2022-03-17T10:00' },
+    { title: 'Product Meeting', startDate: '2022-03-18T14:00', endDate: '2022-03-18T16:00' },
+    { title: 'Send Territory Sales Breakdown', startDate: '2022-03-19T22:00' },
   ];
+
+
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    setState({
+    data: appointments
+    });
+  }, [])
+
+  //console.log(state);
+
+  const commitChanges = ({added, changed, deleted}) => {
+
+    setState((state)=>{
+      let {data} = state;
+      if(added){
+        const startingAddedId = data.length > 0 ? data[data.length -1].id +1 : 0;
+        data = [...data, { id: startingAddedId, ...added }];
+      }
+      if(changed){
+        data = data.map(appointment => (
+          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+      }
+      if(deleted !== undefined){
+        data = data.filter(appointment => appointment.id !== deleted);
+      }
+      return {data};
+    })
+
+  }
   
+  const {data} = state;
 
   return (
-    <Container>
+    <Container >
       <Paper>
-        <Scheduler data={schedulerData}>
-          <ViewState currentDate={currentDate}/>
-        
-      
-          <DayView startDayHour={17} endDayHour={23} />
-
+        <Scheduler
+          data={data}
+          height={660}
+        >
+          <ViewState
+            
+          />
+          <EditingState
+            onCommitChanges={commitChanges}
+          />
+          <IntegratedEditing />
+          <DayView
+            startDayHour={9}
+            endDayHour={19}
+          />
+          <ConfirmationDialog />
           <Appointments />
-
+          <AppointmentTooltip
+            showOpenButton
+            showDeleteButton
+          />
+          <AppointmentForm />
         </Scheduler>
-        
       </Paper>
-
+      
     </Container>
   );
 }
